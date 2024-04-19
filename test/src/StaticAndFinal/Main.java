77a73b13bd8e9414a40343dbd803e3446b90f3c4
king-berry <111548775+king-berry@users.noreply.github.com>
2024-04-19 21:20:47 +0700

    final and static demo

---
+++ test/src/StaticAndFinal/Main.java
@@ -0,0 +1,26 @@
+package StaticAndFinal;
+
+public class Main {
+    // khởi tạo biến và method static
+    public static int staticVar = 10;
+    public static void staticMethod(){
+        System.out.println("static");
+    }
+
+    // khởi tạo biến và method final
+    public final int finalVar = 10;
+    public final void finalMethod(){
+        System.out.println("final");
+    }
+
+
+    public static void main (String[] args){
+        // trụy cập giá trị của static
+        System.out.println("staticVar: " + Main.staticVar);
+        Main.staticMethod();
+        // truy cập giá trị của final
+        Main Obj = new Main();
+        System.out.println("finalVar: " + Obj.finalVar);
+        Obj.finalMethod();
+    }
+}
